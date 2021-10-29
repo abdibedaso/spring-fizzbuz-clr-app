001f12ab36c989cc2d3519a108b71fdafe36f696
Abdi T Bedaso <atbedaso@gmail.com>
2021-10-29 01:44:17 -0500

    init spring app

---
+++ src/test/java/com/geekcolab/development/springfizzbuzclrapp/SpringFizzbuzClrAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekcolab.development.springfizzbuzclrapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFizzbuzClrAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
