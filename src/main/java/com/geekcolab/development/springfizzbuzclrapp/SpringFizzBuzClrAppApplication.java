aab79cf06f8dd4ac321aa53cfddcaf0ef52e2862
Abdi T Bedaso <atbedaso@gmail.com>
2021-10-29 01:45:08 -0500

    imp fizzBuzz

--- src/main/java/com/geekcolab/development/springfizzbuzclrapp/SpringFizzBuzClrAppApplication.java
+++ src/main/java/com/geekcolab/development/springfizzbuzclrapp/SpringFizzBuzClrAppApplication.java
@@ -10,6 +10,24 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class SpringFizzBuzClrAppApplication {
 
+    private static Logger LOG = LoggerFactory.getLogger(SpringFizzBuzClrAppApplication.class);
+
+    @Bean
+    public CommandLineRunner run(){
+        return args -> {
+            for(int i = 1; i <= 100; i++){
+                String result = "";
+
+                result += (i % 3) == 0 ? "Fizz" : "";
+                result += (i % 5) == 0 ? "Buzz" : "";
+
+                System.out.println(result.isEmpty() ? i : result);
+//                LOG.info(result.isEmpty() ? i+"" : result);
+
+            }
+        };
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SpringFizzBuzClrAppApplication.class, args);
     }
